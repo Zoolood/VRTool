17b0ae3b4f957cdde5c0313af4c3b0cfad0d4e4a
Zoolood <122121114+Zoolood@users.noreply.github.com>
2023-08-02 00:51:46 +0200

    Update CombineAndConvert.bat

--- Combine Tool NEW/CombineAndConvert.bat
+++ Combine Tool NEW/CombineAndConvert.bat
@@ -1,4 +1,9 @@
-for /d %%i in (*) do type %%~nxi\*.mp4 > %%~nxi.dat
+@echo off
+
+echo Wait...
+
+for /f %%i in ('dir /B /AD *') do dir /b /s /o:d  %%~nxi\* > %%~nxi.txt 
+for /f %%i in ('dir /B /AD *') do (for /F "tokens=*" %%A in (%%~nxi.txt) do type "%%A") > %%~nxi.dat
 for /f "delims=" %%a in ('dir /B *.dat') do ffmpeg -i "%%a" -vcodec copy -acodec copy "%%~na.ts"
 type *.ts > combined.raw
 ffmpeg -i combined.raw -vcodec copy -acodec copy combined.mp4
