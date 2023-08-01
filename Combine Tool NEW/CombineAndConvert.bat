efa22eefbce61175ea218cbfb534180c063338b8
Zoolood <122121114+Zoolood@users.noreply.github.com>
2023-08-01 19:03:32 +0200

    Update CombineAndConvert.bat

--- Combine Tool NEW/CombineAndConvert.bat
+++ Combine Tool NEW/CombineAndConvert.bat
@@ -1,2 +1,4 @@
-type data\*.mp4 > combined.raw
+for /d %%i in (*) do type %%~nxi\*.mp4 > %%~nxi.dat
+for /f "delims=" %%a in ('dir /B *.dat') do ffmpeg -i "%%a" -vcodec copy -acodec copy "%%~na.ts"
+type *.ts > combined.raw
 ffmpeg -i combined.raw -vcodec copy -acodec copy combined.mp4
